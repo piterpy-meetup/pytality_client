5067232d5fc8d1e2a3ff720113bc67d46c1ce4e5
dima <dudov.dm@gmail.com>
2018-08-18 14:13:58 +0300

    init client

---
+++ pytality/src/main.js
@@ -0,0 +1,33 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import App from './App'
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import VueSocketio from 'vue-socket.io'
+import store from './store'
+
+import HelloWorld from './components/HelloWorld'
+
+Vue.use(Vuetify)
+Vue.use(VueSocketio, 'http://socketserver.com:1923', store);
+
+Vue.config.productionTip = false
+
+const routes = {
+  '/': App,
+}
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  data: {
+    currentRoute: window.location.pathname
+  },
+  computed: {
+    ViewComponent () {
+      return routes[this.currentRoute] || NotFound
+    }
+  },
+  render (h) { return h(this.ViewComponent) }
+})
